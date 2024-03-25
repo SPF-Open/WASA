121d0d48043856bb20df954060a14b9700b404f8
Benoit-Welsch <56845767+Benoit-Welsch@users.noreply.github.com>
2024-03-25 14:35:49 +0000

    Feat : Handle Error + Log

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
+    import Log from "./lib/Rdn/Log.svelte";
     import FileDrop from "./lib/Rdn/FileDrop.svelte";
     import Viewer from "./lib/Rdn/Viewer.svelte";
-
 </script>
 
 <main>
+    <Log />
     <FileDrop />
-    <Viewer/>
+    <Viewer />
 </main>
 
 <style>
-
 </style>
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,6 +1,7 @@
 import { Assessment, parseAssement, parseItem } from "../helper/Parser";
-import { ZipReader, type Entry } from "@zip.js/zip.js";
+import { ZipReader } from "@zip.js/zip.js";
 import { writable } from "svelte/store";
+import logger from "../helper/Log";
 
 export const file = writable<any>(undefined);
 export const assessment = writable<Assessment | undefined>(undefined);
@@ -8,13 +9,19 @@ export const assessment = writable<Assessment | undefined>(undefined);
 file.subscribe(async (zipPath) => {
     if (!zipPath) return
 
-    const reader = new ZipReader(zipPath.stream());
-    const entries = await reader.getEntries();
+    try {
+        const reader = new ZipReader(zipPath.stream());
+        const entries = await reader.getEntries()
+        const assessmentParsed = await parseAssement(entries);
+        const assessmentWithItem = await parseItem(entries, assessmentParsed);
+        assessment.set(assessmentWithItem);
+        logger.log("Assessment loaded");
+    }
+    catch (e) {
+        const err = e as Error
+        logger.catch(err)
+    }
 
-    const assessmentParsed = await parseAssement(entries)
-    const assessmentWithItem = await parseItem(entries, assessmentParsed);
-
-    assessment.set(assessmentWithItem);
 })
 
 export const reset = () => {
